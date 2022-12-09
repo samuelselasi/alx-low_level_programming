f8be67ffb59e08c804dcc69827508c10736f1a8e
samuelselasi <samelsekasi@gmail.com>
2022-12-09 06:32:16 -0800

    fix white space

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,6 +8,7 @@ int main(void)
 {
 	int n1;
 	int n2;
+	
 	for (n1 = 0; n1 < 10; n1++)
 	{
 		for (n2 = n1 + 1; n2 < 10; n2++)
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int n;
+	
 	for (n = 0; n < 10; n++)
 	{
 		putchar(n + '0');
