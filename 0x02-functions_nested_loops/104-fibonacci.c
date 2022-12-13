407bd50a36b52113a3077cac3cda84f38286176b
samuelselasi <85158665+samuelselasi@users.noreply.github.com>
2022-12-13 18:35:37 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,25 +7,24 @@
 int main(void)
 {
 	int i = 2;
-
 	float a = 1;
 	float b = a + 1;
 	float c = a + b;
 
 	printf("%.0f, ", a);
 	printf("%.0f, ", b);
-	while (i <= 98)
+	while (i < 98)
 	{
 		i++;
 		printf("%.0f", c);
 		a = b;
 		b = c;
 		c = a + b;
-		if (i <= 98)
+		if (i < 98)
 		{
 			printf(", ");
 		}
-	}
+	}	 
 	printf("\n");
 	return (0);
 }
