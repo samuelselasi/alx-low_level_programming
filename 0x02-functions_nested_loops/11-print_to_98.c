aad67a999bc5b139a0d95f7e1ad3b7020e5056f4
samuelselasi <samelsekasi@gmail.com>
2022-12-13 06:49:17 -0800

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: input number n 
+ *
+ * Return: none
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+}
