d1324bd2cf42422ca111175e9597926efa3d23c1
samuelselasi <85158665+samuelselasi@users.noreply.github.com>
2022-12-13 13:24:27 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jack_bauer - Entry point
  *
- * Description: function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * Description: prints every minute of the day of Jack Bauer
  *
  * return: none
  */
