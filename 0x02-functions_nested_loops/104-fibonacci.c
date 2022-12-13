6ba55a626fcc763e05f370618c5311ac35d82089
samuelselasi <85158665+samuelselasi@users.noreply.github.com>
2022-12-13 18:40:54 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,24 +6,32 @@
  */
 int main(void)
 {
-	int i = 2;
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
-
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (i < 98)
+	unsigned long int i, j, k, j1, j2, k1, k2;
+	j = 1;
+	k = 2;
+	
+	printf("%lu", j);
+	
+	for (i = 1; i < 91; i++)
 	{
-		i++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (i < 98)
-		{
-			printf(", ");
-		}
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
+	
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+	
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
 	printf("\n");
 	return (0);
