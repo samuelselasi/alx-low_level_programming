722589e527d9e4b3f499fa37ad1831b7c148e633
samuelselasi <samelsekasi@gmail.com>
2022-12-13 10:00:45 -0800

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: none
+ */
+int main(void)
+{
+  int i = 0;
+  long int a = 1;
+  long int b = a;
+  long int c = a + b;
+
+  while (c < 4000000)
+    {
+      if (c % 2 == 0)
+	{
+	  i += c;
+	}
+      a = b;
+      b = c;
+      c = a + b;
+    }
+  printf("%d\n", i);
+  return (0);
+}
