58bed45db8a9bb741018ecfa9f04642839b6aef2
samuelselasi <samelsekasi@gmail.com>
2022-12-14 02:00:10 -0800

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * tests function that prints if integer is positive or negative
+ * main - Entry point tests function that prints if integer is positive or negative
  *
- * Return: Always 0
+ * Return: 0
  */
 
 int main(void)
