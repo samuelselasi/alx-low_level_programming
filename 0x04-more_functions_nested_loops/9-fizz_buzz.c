c70da7e43fd8c291443735eeb60847f0b823b017
samuelselasi <85158665+samuelselasi@users.noreply.github.com>
2022-12-15 14:32:16 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,8 @@
 /**
  * main - main function
  *
- * description - print Fizz, Buzz, FizBuzz
  *
- * Return: 0
+ * Return: a number
  */
 int main(void)
 {
@@ -12,26 +11,22 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf("Fizz");
-		}
-		else if ((i % 5) == 0)
-		{
-			printf("Buzz");
-		}
-		else if ((i % (3 * 5)) == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
 		{
 			printf("%d", i);
 		}
 		if (i != 100)
-		{
 			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
