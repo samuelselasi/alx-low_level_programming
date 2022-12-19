59625ab03cfd5cc8c3c004e82534d7dfdfb32fa2
samuelselasi <85158665+samuelselasi@users.noreply.github.com>
2022-12-19 22:39:10 +0000

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,10 +14,14 @@ int main(void)
 	s = 0;
 	while (s <= 2645)
 	{
-		p = (rand() % 128);
+		p = (rand() % (128 - 32)) + 32;  // Exclude ASCII characters with codes from 1 to 31 and 127
 		s += p;
 		putchar(p);
 	}
 	putchar(2772 - s);
 	return (0);
 }
+
+
+
+
