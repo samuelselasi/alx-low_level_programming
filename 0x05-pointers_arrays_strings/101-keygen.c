f33d1f099567399daf666933c6dcf52882c78176
samuelselasi <85158665+samuelselasi@users.noreply.github.com>
2022-12-19 22:33:54 +0000

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,36 +4,20 @@
 
 /**
  * keygen - generate password
- * @length: the desired length of the password
  * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(void)
 {
-        int p;
-        int s;
-        int length;
-
-        // Check that a length argument was provided
-        if (argc < 2) {
-                fprintf(stderr, "Usage: %s LENGTH\n", argv[0]);
-                return 1;
-        }
-
-        // Parse the length argument
-        length = atoi(argv[1]);
-        if (length <= 0) {
-                fprintf(stderr, "Error: Invalid length\n");
-                return 1;
-        }
-
-        srand(time(NULL));
-        s = 0;
-        while (s < 2772 && length > 0) {
-                p = (rand() % 128);
-                s += p;
-                putchar(p);
-                length--;
-        }
-        putchar(2772 - s);
-        return 0;
+	int p;
+	int s;
+	srand(time(NULL));
+	s = 0;
+	while (s <= 2645)
+	{
+		p = (rand() % 128);
+		s += p;
+		putchar(p);
+	}
+	putchar(2772 - s);
+	return (0);
 }
