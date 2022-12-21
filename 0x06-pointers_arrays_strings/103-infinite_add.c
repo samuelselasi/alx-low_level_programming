5fa08f69f64435c65e86b327ed0fb0d7abc1b44d
samuelselasi <85158665+samuelselasi@users.noreply.github.com>
2022-12-21 23:10:47 +0000

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,44 +1,47 @@
 #include "main.h"
+
 /**
- * infinite_add - add two numbers
+ * infinite_add - adds two numbers
  * @n1: first number
  * @n2: second number
- * @r: result
+ * @r: buffer for result
  * @size_r: buffer size
  *
- * Return: r
+ * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, i = 0, j;
+	int i, j, k, l, m, n;
 
-	while (n1[len1])
-		len1++;
-	while (n2[len2])
-		len2++;
-	while (i < size_r)
-		r[i++] = '0';
-	i = 0;
-	while (len1 > 0 || len2 > 0 || carry > 0)
-	{
-		int d1 = (len1 > 0) ? n1[--len1] - '0' : 0;
-		int d2 = (len2 > 0) ? n2[--len2] - '0' : 0;
-		int sum = d1 + d2 + carry;
-
-		r[i++] = (sum % 10) + '0';
-		carry = sum / 10;
-	}
-	if (i > size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-	j = 0;
-	while (j < i / 2)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		char temp = r[j];
-
-		r[j] = r[i - j - 1];
-		r[i - j - 1] = temp;
-		j++;
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-	r[i] = '\0';
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
 	return (r);
 }
