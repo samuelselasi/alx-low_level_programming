c8136d9108b337f4604c4edcd88d9c68672162f0
samuelselasi <samelsekasi@gmail.com>
2023-01-10 00:54:19 -0800

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - allocates a 2d array of integers
+ * @width: column
+ * @height: row
+ *
+ * Return: 2d array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				free(grid[i]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -64,4 +64,13 @@ Write a function that concatenates two strings.
 * if NULL is passed, treat it as an empty string
 * The function should return NULL on failure
 
+[3-alloc_grid.c](./3-alloc_grid.c)
+```
+Write a function that returns a pointer to a 2 dimensional array of integers.
+```
+* Prototype: int **alloc_grid(int width, int height);
+* Each element of the grid should be initialized to 0
+* The function should return NULL on failure
+* If width or height is 0 or negative, return NULL
+
 
