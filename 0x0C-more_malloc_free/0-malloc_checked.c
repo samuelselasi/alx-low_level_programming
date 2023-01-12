dc02340211463094d2664853404942598a84e1cb
samuelselasi <samelsekasi@gmail.com>
2023-01-11 23:38:15 -0800

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory
+ * @b: unsigned integer
+ * Return: None
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
