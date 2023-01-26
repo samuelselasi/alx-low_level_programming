198436ab8acfdd8031c96849dfcb744638a3934b
samuelselasi <samelsekasi@gmail.com>
2023-01-26 05:51:32 -0800

    add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * exec_before_main - print before main
+ */
+
+void exec_before_main(void) __attribute__ ((constructor));
+
+/**
+ * exec_before_main - print before main
+ */
+
+void exec_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
