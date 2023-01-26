87474009faee4016527255eadb5a8362dba4dd1d
samuelselasi <samelsekasi@gmail.com>
2023-01-26 06:02:59 -0800

    Fix requirement for 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,13 +3,11 @@
 /**
  * exec_before_main - print before main
  */
-
-void exec_before_main(void) __attribute__ ((constructor));
+void exec_before_main(void) __attribute__((constructor));
 
 /**
  * exec_before_main - print before main
  */
-
 void exec_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
