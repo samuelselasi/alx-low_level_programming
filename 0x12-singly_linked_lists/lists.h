55a1226daa7a0246cb248622ca69e1e6576322cc
samuelselasi <samelsekasi@gmail.com>
2023-01-26 01:23:25 -0800

    Update lists.h with prototypes of _strlen and add_node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,5 +22,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+int _strlen(const char *str);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
