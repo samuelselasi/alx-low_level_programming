f0b27ec30dfe5d26541370c291b9aadaa2084790
samuelselasi <samelsekasi@gmail.com>
2023-01-26 00:51:18 -0800

    fix 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,14 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
