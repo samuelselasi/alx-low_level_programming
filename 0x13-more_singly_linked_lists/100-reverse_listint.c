1fdf5d2b565f09eb0752620cec4c5562a247f75e
samuelselasi <samelsekasi@gmail.com>
2023-01-30 10:03:54 -0800

    Add 100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse list
+ * @head: first node
+ * Return: reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous, *next;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	previous = NULL;
+	while (*head != NULL)
+	{
+		next = *head->next;
+		*head->next = previous;
+		previous = *head;
+		*head = next;
+	}
+
+	*head = previous;
+	return (*head);
+}
