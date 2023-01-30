2995513ce483794c2034b7002c34824ff416dc39
samuelselasi <samelsekasi@gmail.com>
2023-01-30 04:14:10 -0800

    add 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,7 +8,7 @@
  * Return: nth node or NULL
  */
 
-get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
