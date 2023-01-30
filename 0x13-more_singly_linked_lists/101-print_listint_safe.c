9897c2e1f0cedbb83f267fdad5ecb12589018c69
samuelselasi <samelsekasi@gmail.com>
2023-01-30 14:46:07 -0800

    Fix task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,14 +11,15 @@ size_t print_listint_safe(const listint_t *head)
 	size_t i = 0;
 	long int j;
 
-	if (head == NULL)
-		exit(98);
-
 	while (head)
 	{
+		if (head == NULL)
+			exit(98);
+
 		j = head - head->next;
 		i++;
 		printf("[%p] %d\n", (void *)head, head->n);
+
 		if (j > 0)
 			head = head->next;
 		else
