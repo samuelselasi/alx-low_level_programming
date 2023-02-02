e8c2ac37eceb71c582c0f291e1499eca034c4168
samuelselasi <samelsekasi@gmail.com>
2023-02-02 00:42:48 -0800

    Add 0-binary_to_unit.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary to ui
+ * @b: the binary number
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b == 49)
+			result = (result << 1) | 1;
+
+		else if (*b == 48)
+			result <<= 1;
+
+		else
+			return (0);
+
+		b++
+	}
+	return (result);
+}
