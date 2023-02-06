8f05e8bcbf4f0d33c226ae9edb78c2f4020df830
samuelselasi <samelsekasi@gmail.com>
2023-02-06 07:36:27 -0800

    fix print_magic.c

--- 0x15-file_io/print_magic.c
+++ 0x15-file_io/print_magic.c
@@ -14,7 +14,7 @@ void print_magic(unsigned char *e_ident)
 
 	for (i = 0; i < EI_NIDENT; i++)
 	{
-		printf("%02x", e_ident[index]);
+		printf("%02x", e_ident[i]);
 		if (i == EI_NIDENT - 1)
 			printf("\n");
 		else
