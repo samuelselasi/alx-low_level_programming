e0250c3b668b4966888e615e0353e3cc2f26ba83
samuelselasi <samelsekasi@gmail.com>
2023-02-06 01:50:31 -0800

    Add main.h header file with prototype of task0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
