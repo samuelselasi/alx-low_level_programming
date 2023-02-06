d11bc9c47ac00b41fc8b915953edfa9af47b4504
samuelselasi <samelsekasi@gmail.com>
2023-02-06 03:50:10 -0800

    Update main.h with prototype for check_elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,8 +9,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#include <elf.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void check_elf(unsigned char *e_ident);
 
 #endif
