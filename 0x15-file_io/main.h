c649373c92553f1f43fcf297b6363997ee813a3d
samuelselasi <samelsekasi@gmail.com>
2023-02-06 05:46:28 -0800

    Update main.h with prototype for print_version

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -22,5 +22,6 @@ void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
 
 #endif
