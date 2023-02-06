d4212286ed50682283d671971b3246471f2010b0
samuelselasi <samelsekasi@gmail.com>
2023-02-06 07:32:52 -0800

    Update main.h header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,14 +14,19 @@
 
 #include <elf.h>
 
+#include "check_elf.c"
+#include "print_magic.c"
+#include "print_class.c"
+#include "print_data.c"
+#include "print_version.c"
+#include "print_abi.c"
+#include "print_osabi.c"
+#include "print_type.c"
+#include "print_entry.c"
+#include "close_elf.c"
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
 
 #endif
