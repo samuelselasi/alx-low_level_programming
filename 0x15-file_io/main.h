250d942f09c89d7f3ab8f3efd11f254bb35ac312
samuelselasi <samelsekasi@gmail.com>
2023-02-06 03:58:55 -0800

    Update main.h with prototype for print_magic

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,5 +16,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
 
 #endif
