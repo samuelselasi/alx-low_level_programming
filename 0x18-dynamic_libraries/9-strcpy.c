ce1d5a9b29da4fb7fcacd056d46f82fbd5bb3292
samuelselasi <samelsekasi@gmail.com>
2023-03-20 02:32:39 -0700

    Add function #20 *_strcpy(char *dest, char *src)

--- 0x18-dynamic_libraries/9-strcpy.c
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -1,25 +1,5 @@
 #include "main.h"
 
-#include "main.h"
-
-/**
-  *  _strlen - print length of string
-  *  @s: the string
-  *  Return: 0
-  */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != 0)
-	{
-		i++;
-	}
-	return (i);
-}
-
 /**
  * char *_strcpy - copy str to src
  * @dest: string destination
