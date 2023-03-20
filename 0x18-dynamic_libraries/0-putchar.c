cc90b1b86a2562caf02d62ab6f7c5201519a9b11
samuelselasi <samelsekasi@gmail.com>
2023-03-20 02:45:55 -0700

    Add function #1 _putchar(char c)

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -1,20 +1,13 @@
-#include "main.h"
-/**
- * main - Entry point
- *
- * Description - print _putchar
- *
- * Return: always 0
- */
-int main(void)
-{
-	char c[] = "_putchar";
-	int i;
+#include <unistd.h>
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
