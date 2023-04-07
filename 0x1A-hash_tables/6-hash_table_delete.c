790aa1c415788c0f22f1124e0b414590613009b7
samuelselasi <samelsekasi@gmail.com>
2023-04-07 09:39:26 +0000

    Add function to delete a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Delete a hash table
+ * @ht: hash table
+ * Return: None
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp_node;
+	unsigned long int i;
+
+	i = 0;
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp_node = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp_node;
+			}
+		}
+		i++;
+	}
+	free(head->array);
+	free(head);
+}
