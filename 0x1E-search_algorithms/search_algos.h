4f79089f8e6708925df8f0a3bd1a796c8fbc0d82
samuelselasi <samelsekasi@gmail.com>
2023-07-11 14:48:04 +0000

    Add prototype of Interpolation search to header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 size_t min(size_t n1, size_t n2);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
