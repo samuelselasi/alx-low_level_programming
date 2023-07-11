3d9809c626ee5d6582a0ef5416e5cfc22c074281
samuelselasi <samelsekasi@gmail.com>
2023-07-11 23:47:58 +0000

    Add prototype of binary search algorithm with recursion to header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search_2(int *array, int value, size_t low, size_t high);
 int advanced_binary(int *array, size_t size, int value);
+int binary_search_3(int *array, int value, size_t low, size_t high);
 
 #endif
