b6270218e23a0676b2efdf09f2375c92cdc239be
samuelselasi <samelsekasi@gmail.com>
2023-07-11 20:56:51 +0000

    Add modified binary search prototype to support exponential search in header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ int jump_search(int *array, size_t size, int value);
 size_t min(size_t n1, size_t n2);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int binary_search_2(int *array, int value, size_t low, size_t high);
 
 #endif
